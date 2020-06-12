79a29b93ccefb4132e9e2e2c1418fe3f24dbcc7b
hakan turğay <turgayh@itu.edu.tr>
2020-06-13 01:55:55 +0300

    Update binarySearch.test.js

--- test/binarySearch.test.js
+++ test/binarySearch.test.js
@@ -1,44 +1,32 @@
 import * as binarySearch from '../algorithm/binarySearch'
+describe("BinarySearch" ,() => {
+    test("binarySearchWithLoops" , () =>{
+        expect(binarySearch.binarySearchWithLoops([], 2)).toBe(-1)
+        expect(binarySearch.binarySearchWithLoops([1], 1)).toBe(0)
+        expect(binarySearch.binarySearchWithLoops([1], 2)).toBe(-1)
+        expect(binarySearch.binarySearchWithLoops([1,2,3], 2)).toBe(1)
+        expect(binarySearch.binarySearchWithLoops([1,2,3], 3)).toBe(2)
+        expect(binarySearch.binarySearchWithLoops([1,2,3], 4)).toBe(-1)
+        expect(binarySearch.binarySearchWithLoops([1,2,3,7], 3)).toBe(2)
 
-test("[], 2",()=>{
-    expect(binarySearch.binarySearchWithLoops([], 2)).toBe(-1)
-    expect(binarySearch.binarySearchWithRecursion([],2)).toBe(-1)
-    expect(binarySearch.binarySearchWithTail([],2)).toBe(-1)
+    })
+    test("binarySearchWithRecursion",()=>{
+        expect(binarySearch.binarySearchWithRecursion([],2)).toBe(-1)
+        expect(binarySearch.binarySearchWithRecursion([1], 1)).toBe(0)
+        expect(binarySearch.binarySearchWithRecursion([1],2)).toBe(-1)
+        expect(binarySearch.binarySearchWithRecursion([1,2,3], 2)).toBe(1)
+        expect(binarySearch.binarySearchWithRecursion([1,2,3], 3)).toBe(2)
+        expect(binarySearch.binarySearchWithRecursion([1,2,3], 4)).toBe(-1)
+        expect(binarySearch.binarySearchWithRecursion([1,2,3,7], 3)).toBe(2)
 
-})
-test("[1], 1",()=>{
-    expect(binarySearch.binarySearchWithLoops([1], 1)).toBe(0)
-    expect(binarySearch.binarySearchWithRecursion([1], 1)).toBe(0)
-    expect(binarySearch.binarySearchWithTail([1], 1)).toBe(0)
-})
-
-test("[1], 2",()=>{
-    expect(binarySearch.binarySearchWithLoops([1], 2)).toBe(-1)
-    expect(binarySearch.binarySearchWithRecursion([1],2)).toBe(-1)
-    expect(binarySearch.binarySearchWithTail([1],2)).toBe(-1)
-})
-
-test("[1,2,3], 2",()=>{
-    expect(binarySearch.binarySearchWithLoops([1,2,3], 2)).toBe(1)
-    expect(binarySearch.binarySearchWithRecursion([1,2,3], 2)).toBe(1)
-    expect(binarySearch.binarySearchWithTail([1,2,3], 2)).toBe(1)
-
-
-})
-test("[1,2,3], 3",()=>{
-    expect(binarySearch.binarySearchWithLoops([1,2,3], 3)).toBe(2)
-    expect(binarySearch.binarySearchWithRecursion([1,2,3], 3)).toBe(2)
-    expect(binarySearch.binarySearchWithTail([1,2,3], 3)).toBe(2)
-
-})
-test("[1,2,3], 4",()=>{
-    expect(binarySearch.binarySearchWithLoops([1,2,3], 4)).toBe(-1)
-    expect(binarySearch.binarySearchWithRecursion([1,2,3], 4)).toBe(-1)
-    expect(binarySearch.binarySearchWithTail([1,2,3], 4)).toBe(-1)
-
-})
-test("[1,2,3,7], 3",()=>{
-    expect(binarySearch.binarySearchWithLoops([1,2,3,7], 3)).toBe(2)
-    expect(binarySearch.binarySearchWithRecursion([1,2,3,7], 3)).toBe(2)
-    expect(binarySearch.binarySearchWithTail([1,2,3,7], 3)).toBe(2)
+    })
+    test("binarySearchWithTail",()=>{
+        expect(binarySearch.binarySearchWithTail([],2)).toBe(-1)
+        expect(binarySearch.binarySearchWithTail([1], 1)).toBe(0)
+        expect(binarySearch.binarySearchWithTail([1],2)).toBe(-1)
+        expect(binarySearch.binarySearchWithTail([1,2,3], 2)).toBe(1)
+        expect(binarySearch.binarySearchWithTail([1,2,3], 3)).toBe(2)
+        expect(binarySearch.binarySearchWithTail([1,2,3], 4)).toBe(-1)
+        expect(binarySearch.binarySearchWithTail([1,2,3,7], 3)).toBe(2)
+    })
 })
